890461dcb55fe37b41d0ee7a7fca1a49908c58e7
Rommel <karlsrommel255@gmail.com>
2023-12-15 13:29:23 -0600

    Python0004

---
+++ main.py
@@ -0,0 +1,45 @@
+myName = input("What's you name?: ")
+myAge = input("How old are you? :")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit?")
+print("OF COURSE YOU DO!")
+
+print()
+print("So, you are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
+print("and clearly think that Replit is")
+print(replit)
+
+
+
+print("Definitely not a Phishing Scam")
+print()
+myName = input ("Your Name")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print("cardNumber")
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+
+
+
+
+
+myName = input("What is your name")
+print("Hello there" + myName)
+myFavFood = input ("What is your favorite food")
+print("its sounds tasty" + myFavFood)
+myFavMusic = input ("What is your favorite music")
+print("I love " + myFavMusic )
+WhereLive = input("Where you live?")
+print("Wow, That's far from here")
